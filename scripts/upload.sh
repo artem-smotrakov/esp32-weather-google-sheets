b568f5797bed56de54739f8807fc783555418b9b
Artem Smotrakov <artem.smotrakov@gmail.com>
2020-11-28 11:22:21 +0100

    Cleanup before uploading in upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# send Ctrl-C
+#echo -e '\003' > /dev/ttyUSB0
+
 # upload code, config and RSA key to esp32
 mpfshell \
     -n -c \
     "open ttyUSB0; put main.conf; put key.json; \
     lcd src; mput .*\.py; \
-    md rsa; cd rsa; lcd rsa; mput .*\.py; \
-    cd ..; md google; cd google; lcd ..; lcd google; mput .*\.py"
+    md rsa; cd rsa; mrm .*; lcd rsa; mput .*\.py; \
+    cd ..; lcd ..; \
+    md google; cd google; mrm .*; lcd google; mput .*\.py; \
+    cd ..; lcd ..; \
+    md http; cd http; mrm .*; lcd http; mput .*\.py; "
