71e10db97a262cf21c4dac0d164a5ab42910d2c3
Artem Smotrakov <artem.smotrakov@gmail.com>
2020-11-28 12:13:29 +0100

    Read SSID and password from env in upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# store the original config
+cp main.conf main.conf.original
+
+if [ "$SSID" != "" ]; then
+  jq ".ssid = \"${SSID}\"" main.conf > tmp
+  mv tmp main.conf
+else
+  echo "SSID env variable is empty which means that no WiFi network identifier is written in the config."
+  echo "Your device will not be able to connect to WiFi."
+  echo "Please set SSID env variable."
+fi
+
+if [ "$WIFI_PASSWORD" != "" ]; then
+  jq ".password = \"$WIFI_PASSWORD\"" main.conf > tmp
+  mv tmp main.conf
+else
+  echo "WIFI_PASSWORD env variable is empty which means that no WiFi password is written in the config."
+  echo "Your device will not be able to connect to WiFi."
+  echo "Please set WIFI_PASSWORD env variable."
+fi
+
 # send Ctrl-C
 #echo -e '\003' > /dev/ttyUSB0
 
@@ -13,3 +34,6 @@ mpfshell \
     md google; cd google; mrm .*; lcd google; mput .*\.py; \
     cd ..; lcd ..; \
     md http; cd http; mrm .*; lcd http; mput .*\.py; "
+
+# restore the original config
+mv main.conf.original main.conf
