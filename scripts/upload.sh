1aa19215d38cea986e57b34a0efcb9aef75b5691
Artem Smotrakov <artem.smotrakov@gmail.com>
2020-11-28 15:10:59 +0100

    Allow configuring parameters for spreadsheet via env variables
    Fixed #9

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -10,7 +10,6 @@ else
   echo "SSID env variable is empty which means that no WiFi network identifier is written in the config."
   echo "Your device will not be able to connect to WiFi."
   echo "Please set SSID env variable."
-  exit 1
 fi
 
 if [ "$WIFI_PASSWORD" != "" ]; then
@@ -20,7 +19,6 @@ else
   echo "WIFI_PASSWORD env variable is empty which means that no WiFi password is written in the config."
   echo "Your device will not be able to connect to WiFi."
   echo "Please set WIFI_PASSWORD env variable."
-  exit 1
 fi
 
 if [ "$ACCESS_POINT_SSID" != "" ]; then
@@ -30,7 +28,6 @@ else
   echo "ACCESS_POINT_SSID env variable is empty."
   echo "It means that a WiFi access point can't be started for configuring the device."
   echo "Please set ACCESS_POINT_SSID env variable."
-  exit 1
 fi
 
 if [ "$ACCESS_POINT_PASSWORD" != "" ]; then
@@ -40,7 +37,24 @@ else
   echo "ACCESS_POINT_PASSWORD env variable is empty."
   echo "It means that a WiFi access point can't be started for configuring the device."
   echo "Please set ACCESS_POINT_PASSWORD env variable."
-  exit 1
+fi
+
+if [ "$GOOGLE_SERVICE_ACCOUNT_EMAIL" != "" ]; then
+  jq ".google_service_account_email = \"$GOOGLE_SERVICE_ACCOUNT_EMAIL\"" main.conf > tmp
+  mv tmp main.conf
+else
+  echo "GOOGLE_SERVICE_ACCOUNT_EMAIL env variable is empty."
+  echo "It means that the device won't be able to write data to a spreadsheet."
+  echo "Please set GOOGLE_SERVICE_ACCOUNT_EMAIL env variable."
+fi
+
+if [ "$GOOGLE_SHEET_ID" != "" ]; then
+  jq ".google_sheet_id = \"$GOOGLE_SHEET_ID\"" main.conf > tmp
+  mv tmp main.conf
+else
+  echo "GOOGLE_SHEET_ID env variable is empty."
+  echo "It means that the device won't be able to write data to a spreadsheet."
+  echo "Please set GOOGLE_SHEET_ID env variable."
 fi
 
 # send Ctrl-C
