adc0b3d4a94b0c8389de76d3f441664e3ca8ea5c
Artem Smotrakov <artem.smotrakov@gmail.com>
2020-11-29 22:50:05 +0100

    Measure CO2 with MH-Z19B sensor
    - Added MHZ19BSensor class
    - Added new parameters to config to specify
      pins for MH-Z19B
    - Send CO2 measurements to a Google sheet
    
    Fixes #14

--- src/weather.py
+++ src/weather.py
@@ -1,3 +1,4 @@
+from machine import UART
 import time
 import dht
 import machine
@@ -57,3 +58,50 @@ class DHT22Sensor:
         print('farenheit   = %.2f' % f)
         print('humidity    = %.2f' % h)
         return [c, h, f]
+
+
+# this class measures CO2 with MH-Z19B sensor
+class MHZ19BSensor:
+
+    # initializes a new instance
+    def __init__(self, tx_pin, rx_pin, lights):
+        self.uart = UART(1, baudrate=9600, bits=8, parity=None, stop=1, tx=int(tx_pin), rx=int(rx_pin))
+        self.lights = lights
+
+    # measure CO2
+    def measure(self):
+        while True:
+
+            # send a read command to the sensor
+            self.uart.write(b'\xff\x01\x86\x00\x00\x00\x00\x00\x79')
+
+            # a little delay to let the sensor measure CO2 and send the data back
+            time.sleep(1)  # in seconds
+
+            # read and validate the data
+            buf = self.uart.read(9)
+            if self.is_valid(buf):
+                break
+
+            # retry if the data is wrong
+            self.lights.error_on()
+            print('error while reading MH-Z19B sensor: invalid data')
+            print('retry ...')
+
+        self.lights.error_off()
+        co2 = buf[2] * 256 + buf[3]
+        print('co2         = %.2f' % co2)
+        return [co2]
+
+    # check data returned by the sensor
+    def is_valid(self, buf):
+        if buf is None or buf[0] != 0xFF or buf[1] != 0x86:
+            return False
+        i = 1
+        checksum = 0x00
+        while i < 8:
+            checksum += buf[i] % 256
+            i += 1
+        checksum = ~checksum & 0xFF
+        checksum += 1
+        return checksum == buf[8]
