842ab014a3e2d43a4bbac06e2544b37324bae691
Artem Smotrakov <artem.smotrakov@gmail.com>
2021-01-02 23:41:51 +0100

    Decode posted data in ConnectionHandler

--- src/settings.py
+++ src/settings.py
@@ -73,6 +73,54 @@ FORM_TEMPLATE = """\
 </html>
 """
 
+_hextobyte_cache = None
+
+
+# borrowed from https://forum.micropython.org/viewtopic.php?t=3076
+def unquote(string):
+    """unquote('abc%20def') -> b'abc def'."""
+    global _hextobyte_cache
+
+    # Note: strings are encoded as UTF-8. This is only an issue if it contains
+    # unescaped non-ASCII characters, which URIs should not.
+    if not string:
+        return b''
+
+    if isinstance(string, str):
+        string = string.encode('utf-8')
+
+    bits = string.split(b'%')
+    if len(bits) == 1:
+        return string
+
+    res = [bits[0]]
+    append = res.append
+
+    # Build cache for hex to char mapping on-the-fly only for codes
+    # that are actually used
+    if _hextobyte_cache is None:
+        _hextobyte_cache = {}
+
+    for item in bits[1:]:
+        try:
+            code = item[:2]
+            char = _hextobyte_cache.get(code)
+            if char is None:
+                char = _hextobyte_cache[code] = bytes([int(code, 16)])
+            append(char)
+            append(item[2:])
+        except KeyError:
+            append(b'%')
+            append(item)
+
+    return b''.join(res)
+
+
+# decode a url-encoded string:
+def url_decode(string):
+    """url_decode('abc%20def') -> 'abc def'."""
+    return unquote(string).decode('utf-8')
+
 
 # returns an HTTP response with the form above
 # the fields in the form contain the current configuration
@@ -122,7 +170,7 @@ class ConnectionHandler:
                     if name == 'password' and not value:
                         continue
 
-                    self._config.set(name, value)
+                    self._config.set(name, url_decode(value))
 
                 # store the config
                 self._config.store()
